83b50b8361eb64478c2a294933dd81755b3ce431
wenshao <szujobs@hotmail.com>
2012-08-21 20:55:31 +0800

    init project

---
+++ src/main/java/com/alibaba/druid/monitor/entity/AppFollower.java
@@ -0,0 +1,36 @@
+package com.alibaba.druid.monitor.entity;
+
+import java.util.Date;
+
+public class AppFollower {
+
+    private long appId;
+    private long followerId;
+
+    private Date createdTime;
+
+    public long getAppId() {
+        return appId;
+    }
+
+    public void setAppId(long appId) {
+        this.appId = appId;
+    }
+
+    public long getFollowerId() {
+        return followerId;
+    }
+
+    public void setFollowerId(long followerId) {
+        this.followerId = followerId;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+}
